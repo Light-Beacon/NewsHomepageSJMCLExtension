df7a1a4111184dd1df4553a3a19dfbd3457f771e
Nattiden <2682645990@qq.com>
2026-04-21 16:13:36 +0800

    debug-console

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import type { ExtensionFactoryApi } from "./types/sjmcl";
 import { createMcVersionDetailPage } from "./pages/mc-version-detail";
 import { createSettingsPage } from "./pages/settings-page";
 import { createNewsWidget } from "./widgets/news";
+import { createConsoleWidget } from "./widgets/console";
+
+const debugMode = true;
 
 (function registerExampleExtension(factory) {
   const token = document.currentScript?.dataset?.extensionToken || "";
@@ -16,9 +19,7 @@ import { createNewsWidget } from "./widgets/news";
 
   window.registerExtension(factory, token);
 })(function createExtension(api: ExtensionFactoryApi) {
-  return {
-    homeWidgets: [
-      {
+  const newsWidget = {
         title: "新闻主页",
         icon: api.resolveAssetUrl("assets/icons/news.png"),
         description: "提供最新的 Minecraft 中文新闻",
@@ -26,8 +27,22 @@ import { createNewsWidget } from "./widgets/news";
         defaultWidth: 420,
         minWidth: 400,
         key: "news-homepage-widget",
-      }
-    ],
+      };
+  const consoleWidget = {
+        title: "控制台",
+        icon: api.resolveAssetUrl("assets/icons/console.png"),
+        description: "调试台",
+        Component: createConsoleWidget(api),
+        defaultWidth: 420,
+        minWidth: 400,
+        key: "console-widget",
+      };
+  const widgets = [newsWidget];
+  if(debugMode){
+    widgets.push(consoleWidget);
+  }
+  return {
+    homeWidgets: widgets,
     settingsPage: {
       Component: createSettingsPage(api),
     },
